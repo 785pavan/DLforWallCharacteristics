eef00d1efcdaa7776b4a9fe9d407cd6ca79962f7
Pavan Kumar Kandapagari <40955141+pkkandapagari@users.noreply.github.com>
2019-09-24 13:22:41 +0200

    Add files via upload

---
+++ model_basic/renamer.py
@@ -0,0 +1,14 @@
+import os
+
+i = 0
+filenames = next(os.walk(os.getcwd()))[2]
+filenames.remove(os.path.basename(__file__))
+for filename in filenames:
+    dst = "image." + str(i) + ".tif"
+    src = os.getcwd() + '\\' + filename
+    dst = os.getcwd() + '\\' + dst
+    # rename() function will
+    # rename all the files
+    os.rename(src, dst)
+    i += 1
+print('done')
